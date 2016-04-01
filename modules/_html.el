6d8b00eb76a3afd02fb8566569753ebe6fdbf904
Simon Welker <simon.welker@esirion.de>
2016-04-01 16:38:08 +0200

    Add Emmet to html-mode

--- modules/_html.el
+++ modules/_html.el
@@ -1,7 +1,20 @@
+(require-package 'emmet-mode)
+
+;; Basic settings
 (add-hook 'html-mode-hook
   (lambda ()
-    ; set offset and tab width to 4
-    (setq sgml-basic-offset 4
-	  tab-width 4)
-    ; Use tabs for indendation
-    (setq indent-tabs-mode t)))
+    (setq
+     ; Use tabs for indendation
+     sgml-basic-offset 4
+     tab-width 4
+     indent-tabs-mode t)))
+
+;; Emmet
+(require 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode)
+;(add-hook 'css-mode-hook 'emmet-mode)
+
+(eval-after-load "emmet-mode"
+  '(progn
+     (define-key emmet-mode-keymap (kbd "C-j") nil)
+     (define-key emmet-mode-keymap (kbd "C-<tab>") 'emmet-expand-line)))
