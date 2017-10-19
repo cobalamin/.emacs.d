4a0b09b47cc727590e24541f12d9c6f58c28254a
Simon Welker <simon.welker@outlook.com>
2017-10-19 13:46:48 +0200

    JS/JSX update

--- modules/_js.el
+++ modules/_js.el
@@ -1,7 +1,17 @@
 ;; Basic settings
 (add-hook 'js-mode-hook
   (lambda ()
-    ; Use tabs for indendation
-    (setq indent-tabs-mode t
+    ; Use spaces for indendation
+    (setq indent-tabs-mode nil
 	  tab-width 2
 	  js-indent-level 2)))
+
+;; JSX
+(add-hook 'rjsx-mode-hook
+  '(lambda () (progn
+    (setq indent-tabs-mode nil
+	  tab-width 2
+	  js2-basic-offset 2))))
+
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . jsx-mode))
+(autoload 'jsx-mode "jsx-mode" "JSX mode" t)
